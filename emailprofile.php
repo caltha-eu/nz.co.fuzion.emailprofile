5975aecb67057dc7e59505ee4a8848f6aa06505e
Artur Śmigielski <kontakt@iprosoft.pl>
2022-09-29 13:08:47 +0200

    PHP 7.4 upgrade, civix 22.08

--- emailprofile.php
+++ emailprofile.php
@@ -124,17 +124,6 @@ function emailprofile_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function emailprofile_civicrm_xmlMenu(&$files) {
-  _emailprofile_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -187,54 +176,6 @@ function emailprofile_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function emailprofile_civicrm_managed(&$entities) {
-  _emailprofile_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function emailprofile_civicrm_caseTypes(&$caseTypes) {
-  _emailprofile_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function emailprofile_civicrm_angularModules(&$angularModules) {
-_emailprofile_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function emailprofile_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _emailprofile_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Functions below this ship commented out. Uncomment as required.
  *
 
@@ -243,8 +184,23 @@ function emailprofile_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function emailprofile_civicrm_preProcess($formName, &$form) {
-
-}
 
 */
+
+// /**
+//  * Implements hook_civicrm_postInstall().
+//  *
+//  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+//  */
+// function emailprofile_civicrm_postInstall() {
+//   _emailprofile_civix_civicrm_postInstall();
+// }
+
+// /**
+//  * Implements hook_civicrm_entityTypes().
+//  *
+//  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+//  */
+// function emailprofile_civicrm_entityTypes(&$entityTypes) {
+//   _emailprofile_civix_civicrm_entityTypes($entityTypes);
+// }
